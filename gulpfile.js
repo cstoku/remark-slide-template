51c48a69dc66cd5beefa19fb427e881fb7306dc0
cstoku <cs_toku@sce-toku.jp>
2016-09-13 04:40:23 +0900

    add node-sass & gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
  
 var browserSync = require("browser-sync");
 var exec = require('child_process').exec
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
 
 gulp.task("browserSyncTask", function () {
     browserSync({
@@ -10,10 +17,10 @@ gulp.task("browserSyncTask", function () {
         }
     });
  
-    //gulp.watch("source/**", function() {
-    //    exec('make html')
-    //});
-    gulp.watch("./**", browserSync.reload);
+    gulp.watch("./**/*.html", browserSync.reload);
+    gulp.watch("./**/*.md", browserSync.reload);
+    gulp.watch('./css/**/*.css', browserSync.reload);
+    gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 gulp.task('default', ['browserSyncTask']);
